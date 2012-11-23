10b663f9e9dbce3ac7109c86ab8a02ea8e30e59b
Ato ARAKI <ato.araki@gmail.com>
2012-11-23 15:53:06 +0900

    Add sample code

---
+++ newmath/sqrt.go
@@ -0,0 +1,13 @@
+// Package newmath is a trivial example package.
+package newmath
+
+// Sqrt returns an approximation to the square root of x.
+func Sqrt(x float64) float64 {
+	// This is a terrible implementation.
+	// Real code should import "math" and use math.Sqrt.
+	z := 0.0
+	for i := 0; i < 1000; i++ {
+		z -= (z*z - x) / (2 * x)
+	}
+	return z
+}
